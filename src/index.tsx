b1668cdd4efae01f4187d85e04b710fd0865824c
Seh Hui, Leong <felixleong@gmail.com>
2017-10-22 10:29:12 +0800

    Added SVG.js sample into the code base, and we can now actually start coding.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import * as SVG from 'svg.js'
 
 import { Hello } from './components/Hello'
 
@@ -12,3 +13,15 @@ ReactDOM.render(
   <Hello compiler="Typescript" framework="React" />,
   component
 )
+
+//-----
+
+// Setting up the page (again)
+component = document.createElement('div')
+component.id = 'drawing'
+document.body.appendChild(component)
+
+// Draw some SVG
+let draw = SVG('drawing')
+let rect = draw.rect(100, 100).fill('#f06')
+let rect2 = draw.rect(100, 100).move(100, 0).fill('#06f')
