31e29a87475ce7c5a9e600622a5db5eb3ec2ab2e
Seh Hui, Leong <felixleong@gmail.com>
2017-10-23 22:27:27 +0800

    Work-in-progress.

--- src/index.tsx
+++ src/index.tsx
@@ -25,3 +25,23 @@ document.body.appendChild(component)
 let draw = SVG('drawing')
 let rect = draw.rect(100, 100).fill('#f06')
 let rect2 = draw.rect(100, 100).move(100, 0).fill('#06f')
+
+// Back pocket
+draw.path(
+  'M0 0 ' +
+  'v 88 h 63 v -88 ' +
+  'h -20 a 10 10 0 0 1 -23 0 ' +
+  'h -20 z')
+  .fill('none')
+  .stroke({ color: '#333', width: 1 })
+
+// Top flap
+draw.path(
+  'M 0 0 ' +
+  'v 37 h 63 v -37 ' +
+  'a 8 8 -90 0 0 -8 -8 ' +
+  'h -47 ' +
+  'a 8 8 90 0 0 -8 8 z')
+  .move(100, 0)
+  .fill('none')
+  .stroke({ color: '#333', width: 1 })
